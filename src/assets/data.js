e2e2061ec271122709016bf90e8003936776735a
NivedhaE <nivedhaezhilarasan@gmail.com>
2024-11-28 15:52:33 +0530

    quiz app

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data =[
+    {
+        question:"What is the purpose of the <head> element in an HTML document?",
+        option1:"Display content",
+        option2:"Meta-information",
+        option3:"Link websites",
+        option4:"Navigation",
+        ans:2
+    },
+    {
+        question:"How do you create a hyperlink in HTML?",
+        option1:"<link>",
+        option2:"<a src>",
+        option3:"<a href>",
+        option4:"<hyperlink>",
+        ans:3
+    },
+    {
+        question:"What are semantic HTML elements, and why are they important?",
+        option1:"No meaning",
+        option2:"Style webpage",
+        option3:"Describe meaning",
+        option4:"Navigation",
+        ans:3
+    },
+    {
+        question:"What is the difference between class and id selectors in CSS?",
+        option1:"Single vs. multiple",
+        option2:"Hash vs. dot",
+        option3:"Multiple vs. single",
+        option4:"Text vs. containers",
+        ans:3
+    },
+    {
+        question:"How do you center a block element horizontally using CSS?",
+        option1:"text-align",
+        option2:"margin: 0",
+        option3:"flex-center",
+        option4:"margin: auto",
+        ans:4
+    }
+    
+]
