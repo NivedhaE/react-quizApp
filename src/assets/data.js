66dd9f2155dbcda77936c6b37b7a82c0478b83b8
NivedhaE <nivedhaezhilarasan@gmail.com>
2024-12-08 14:11:46 +0530

    quiz

---
+++ src/assets/data.js
@@ -0,0 +1,59 @@
+export const data =[
+    {
+        question:"What is the purpose of the <head> element in an HTML document?",
+        option1:"Display content",
+        option2:"Meta-information",
+        option3:"Link websites",
+        option4:"Navigation",
+        ans:2
+    },
+    {
+        question:"How do you create a hyperlink in HTML?",
+        option1:"<link>",
+        option2:"<a src>",
+        option3:"<a href>",
+        option4:"<hyperlink>",
+        ans:3
+    },
+    {
+        question:"What are semantic HTML elements, and why are they important?",
+        option1:"No meaning",
+        option2:"Style webpage",
+        option3:"Describe meaning",
+        option4:"Navigation",
+        ans:3
+    },
+    {
+        question:"What is the difference between class and id selectors in CSS?",
+        option1:"Single vs. multiple",
+        option2:"Hash vs. dot",
+        option3:"Multiple vs. single",
+        option4:"Text vs. containers",
+        ans:3
+    },
+    {
+        question:"How do you center a block element horizontally using CSS?",
+        option1:"text-align",
+        option2:"margin: 0",
+        option3:"flex-center",
+        option4:"margin: auto",
+        ans:4
+    },
+    {
+        question:"What is the purpose of the useState hook in React?",
+        option1:"To manage the lifecycle of a component",
+        option2:"To fetch data from an API",
+        option3:"To add CSS styles to a component",
+        option4:"To manage state within a functional component",
+        ans:4
+    },
+    {
+        question:"What is JSX in React?",
+        option1:"A JavaScript function used to create components",
+        option2:"A syntax extension that allows mixing HTML with JavaScript",
+        option3:"A library for handling HTTP requests in React",
+        option4:"A tool for optimizing React applications",
+        ans:2
+    }
+    
+]
